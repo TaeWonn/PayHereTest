46e0eb504ef60f740994b79eea1426c7001fe8bd
TaeWonn <xodns9485@gmail.com>
2022-06-29 08:22:28 +0900

    Initial commit

---
+++ src/main/kotlin/kr/payhere/payheretest/PayHereTestApplication.kt
@@ -0,0 +1,11 @@
+package kr.payhere.payheretest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PayHereTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<PayHereTestApplication>(*args)
+}
---
+++ src/test/kotlin/kr/payhere/payheretest/PayHereTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.payhere.payheretest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PayHereTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
